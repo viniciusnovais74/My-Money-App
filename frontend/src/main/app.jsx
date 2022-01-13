9ce98f9881e1e1c9348eb8626c3f55a9d8e5e2ad
viniciusnovais74 <viniciusnovais74@gmail.com>
2022-01-13 13:30:39 -0300

    Project FINISH

--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -5,7 +5,7 @@ import React from 'react'
 import Header from '../common/template/header'
 import SideBar from '../common/template/sideBar'
 import Footer from '../common/template/footer'
-import Routes from './routes'
+
 import Messages from '../common/msg/messages'
 
 
@@ -14,7 +14,7 @@ export default props => (
         <Header />
         <SideBar />
         <div className="content-wrapper">
-            <Routes/>
+            {props.children}
         </div>
         <Footer />
         <Messages/>
